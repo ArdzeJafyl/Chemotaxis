232b3b72080ef7a1dc2fc0c22ef8827820535b3b
ArdzeJafyl <ansimmons.gt@gmail.com>
2015-10-13 10:20:44 -0700

    finished

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,9 +1,25 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  int BacNum = 1000;
  Bacteria [] microbe;
 
- void setup()   
+ public void setup()   
  {     
- 	size(800,800);
+ 	
  	background(0);
  	microbe = new Bacteria[BacNum];
  	for(int i = 0; i < microbe.length;i++){
@@ -11,7 +27,7 @@
     }   
  }
 
- void draw()   
+ public void draw()   
  {    
   	fill(0,0,0,10);
   	noStroke();
@@ -34,7 +50,7 @@
  		call = 0;
  	}
 
- 	void move(){
+ 	public void move(){
 		if(call == 20){
 			d = (int)(Math.random()*4);
 			call = 0;
@@ -61,7 +77,7 @@
 		}
  	}
 
- 	void show(){
+ 	public void show(){
  		fill(0);
  		strokeWeight(2);
  		stroke(photons);
@@ -78,3 +94,13 @@
 
 
 
+  public void settings() { 	size(800,800); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
